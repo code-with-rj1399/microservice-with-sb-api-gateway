0b051c6ab56644eeff202d693947337db1135a5e
Rajesh <rajesh4.ramesh@gmail.com>
2025-03-28 16:41:08 +0530

    intial commit

---
+++ src/main/java/com/microservices_with_sb/microservice_with_sb_api_gateway/MicroserviceWithSbApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.microservices_with_sb.microservice_with_sb_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceWithSbApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceWithSbApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservices_with_sb/microservice_with_sb_api_gateway/MicroserviceWithSbApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices_with_sb.microservice_with_sb_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceWithSbApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
